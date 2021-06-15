a92e94e43ca198594959520abf56876d10c9d8bb
Madalena <20200154@iade.pt>
2021-06-15 18:38:27 +0100

    Initial Commit

---
+++ src/main/java/com/iade/YouChoose1/YouChoose1Application.java
@@ -0,0 +1,13 @@
+package com.iade.YouChoose1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouChoose1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouChoose1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iade/YouChoose1/YouChoose1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iade.YouChoose1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouChoose1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
